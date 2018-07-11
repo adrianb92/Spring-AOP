bb696de75856ef91d43440c2dbb4755b10c4ac5e
adrianb92 <adrian.barczuk@gmail.com>
2018-07-11 23:57:35 +0200

    Add @AfterReturnig advice

---
+++ spring-aop-demo-afterreturning/src/com/adrianbarczuk/spring/aop/MainDemoApp.java
@@ -0,0 +1,36 @@
+package com.adrianbarczuk.spring.aop;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.adrianbarczuk.spring.aop.dao.AccountDAO;
+import com.adrianbarczuk.spring.aop.dao.MembershipDAO;
+
+public class MainDemoApp {
+	
+	public static void main(String[] args) {
+		
+		//read spring config java class
+		AnnotationConfigApplicationContext context =
+				new AnnotationConfigApplicationContext(DemoConfig.class);
+		
+		//get beans from container
+		AccountDAO accountDAO = context.getBean("accountDAO", AccountDAO.class);
+		MembershipDAO membershipDAO = context.getBean("membershipDAO", MembershipDAO.class);
+		
+		//call business methods
+		Account account = new Account();
+		account.setName("Nameaaaaa");
+		account.setLevel("Gold");
+		accountDAO.addAccount(account, true);
+		accountDAO.doWork();
+		accountDAO.setName("foo");
+		System.out.println(accountDAO.getName());
+		
+		membershipDAO.addMember();
+		
+		//close context
+		context.close();
+		
+	}
+
+}
---
+++ spring-aop-demo-afterreturning/src/com/adrianbarczuk/spring/aop/dao/AccountDAO.java
@@ -0,0 +1,60 @@
+package com.adrianbarczuk.spring.aop.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.adrianbarczuk.spring.aop.Account;
+
+@Component
+public class AccountDAO {
+
+	private String name;
+	private String serviceCode;
+	
+	public List<Account> findAccounts() {
+		
+		List<Account> accounts = new ArrayList<>();
+		
+		Account account1 = new Account("First", "Silver");
+		Account account2 = new Account("Second", "Platinum");
+		Account account3 = new Account("Third", "Gold");
+		
+		accounts.add(account1);
+		accounts.add(account2);
+		accounts.add(account3);
+		
+		return accounts;
+	}
+	
+	public void addAccount(Account account, boolean vipFlag) {
+		
+		System.out.println("ADD ACCOUNT in " + getClass());
+	}
+	
+	public boolean doWork() {
+		
+		System.out.println("DO WORK in " + getClass());
+		
+		return true;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getServiceCode() {
+		return serviceCode;
+	}
+
+	public void setServiceCode(String serviceCode) {
+		this.serviceCode = serviceCode;
+	}
+	
+	
+}
